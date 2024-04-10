7ea5d697cf864b943167ce256fd5c766505e3cff
Elyndrium <91188350+Elyndrium@users.noreply.github.com>
2024-04-10 17:16:07 +0200

    Good concurrency on pixels (~13s to ~2s for 4K)

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "stb_image.h"
 
 #include <iostream>
+#include <thread>
 
 #define PI 3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679821
 
@@ -228,6 +229,22 @@ Vector get_color(std::vector<Sphere> Scene, std::vector<Light> Lights, Ray pr, i
     return color;
 }
 
+void concurrent_line(std::vector<Sphere> Scene, std::vector<Light> Lights, int W, int H, int i0, size_t block_size, std::vector<unsigned char> &image){
+    for (int i = i0; i < i0+block_size; i++){
+        for (int j = 0; j < W; j++) {
+            Ray pr = pixel_ray(W, H, i, j);
+            Vector color = Vector(0,0,0);
+            color = get_color(Scene, Lights, pr);
+                
+            //std::cout << color[0] << " " << color[1] << " " << color[2] << std::endl;
+            gamma_correction(color);
+            image[(i * W + j) * 3 + 0] = color.data[0];
+            image[(i * W + j) * 3 + 1] = color.data[1];
+            image[(i * W + j) * 3 + 2] = color.data[2];
+        }
+    }
+}
+
 int main(){
     Vector empty_vec = Vector(-1, -1, -1);
     std::vector<Sphere> Scene{  Sphere(Vector(0,0,0), 10, Vector(170, 10, 170)),        // center ball
@@ -246,17 +263,27 @@ int main(){
 
     place_camera_scene(Scene, Lights, Vector(0, 0, 55));
 
-    int W = 512;
-    int H = 512;
+    int W = 1024;
+    int H = 1024;
  
     std::vector<unsigned char> image(W * H * 3, 0);
 
-    for (int i = 0; i < H; i++) {
+    size_t n_threads = 48;
+
+    size_t block_size = H / n_threads;
+
+    std::vector<std::thread> threads(n_threads);
+    
+    for (int i = 0; i < n_threads-1; i++) {
+        threads[i] = std::thread(&concurrent_line, Scene, Lights, W, H, i*block_size, block_size, std::ref(image));
+    }
+
+    for (int i = (n_threads-1)*block_size; i < W; i++){
         for (int j = 0; j < W; j++) {
             Ray pr = pixel_ray(W, H, i, j);
             Vector color = Vector(0,0,0);
             color = get_color(Scene, Lights, pr);
-            
+                
             //std::cout << color[0] << " " << color[1] << " " << color[2] << std::endl;
             gamma_correction(color);
             image[(i * W + j) * 3 + 0] = color.data[0];
@@ -264,7 +291,10 @@ int main(){
             image[(i * W + j) * 3 + 2] = color.data[2];
         }
     }
-    
+
+    for (int i = 0; i < n_threads-1; i++){
+        threads[i].join();
+    }
 
     stbi_write_png("image.png", W, H, 3, &image[0], 0);
  
